b0ab0a368f0c4ae08d197422a6b246c0a70680fb
LazyCorpz <lazycorpze@gmail.com>
2025-01-11 02:41:28 -0500

    Create spring boot project

---
+++ src/test/java/io/projectnuo/nuo_backend/NuoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.projectnuo.nuo_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NuoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
